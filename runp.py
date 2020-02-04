f04062b706044492897fb33917ec799f6a84b181
sbibek <sbibek@hotmail.com>
2020-02-04 15:15:57 -0800

    fix

--- runp.py
+++ runp.py
@@ -1,10 +1,12 @@
 import sys
 import subprocess
+import time
 
 procs = []
-for i in range(200):
+for i in range(10):
     proc = subprocess.Popen([sys.executable, 'client.py'])
     procs.append(proc)
+    time.sleep(0.2)
 
 for proc in procs:
     proc.wait()
